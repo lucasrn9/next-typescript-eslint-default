a9b1ea41048e8454f587b8d59003207a7ae78f60
Lucas Ribeiro <lucas.rn00@gmail.com>
2022-07-29 10:50:50 -0300

    finish setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +15,6 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
@@ -66,7 +64,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
